96811d030555bdeb821f64b1fbf9c1ccd90b831f
Dar Levy <darlevy8@gmail.com>
2024-06-03 18:59:27 +0300

    Create Profile form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import "./App.css";
 import Profiles from "./components/profiles";
+import { Route } from "react-router-dom";
 
 function App() {
-    return (
-        <div className="App">
-            <Profiles/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Route path="/" component={Profiles} />
+    </div>
+  );
 }
 
 export default App;
