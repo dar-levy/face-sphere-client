0f7ffcc4c7146fef5e1c8de2f08e320339f8930c
Dar Levy <darlevy8@gmail.com>
2024-06-03 15:20:06 +0300

    Implement conditional rendering

--- src/components/profiles.jsx
+++ src/components/profiles.jsx
@@ -12,30 +12,37 @@ class Profiles extends Component {
     }
 
     render() {
+        const {length: count} = this.state.profiles
+        if (count === 0) return <p>There are on profiles in the database</p>
+
         return (
-            <table className="table">
-                <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th/>
-                </tr>
-                </thead>
-                <tbody>
-                {this.state.profiles.map(profile => (
-                    <tr key={profile.id}>
-                        <td>{profile.first_name}</td>
-                        <td>{profile.last_name}</td>
-                        <td>{profile.email}</td>
-                        <td>
-                            <button onClick={() => this.handleDelete(profile)} className="btn btn-danger btn-sm">Delete
-                            </button>
-                        </td>
+            <>
+                <p>Showing {count} profiles in the database</p>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th/>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {this.state.profiles.map(profile => (
+                        <tr key={profile.id}>
+                            <td>{profile.first_name}</td>
+                            <td>{profile.last_name}</td>
+                            <td>{profile.email}</td>
+                            <td>
+                                <button onClick={() => this.handleDelete(profile)}
+                                        className="btn btn-danger btn-sm">Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </>
         );
     }
 }
