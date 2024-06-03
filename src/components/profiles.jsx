bba2106b2b7b3075e97c04aec66e164bacd575c4
Dar Levy <darlevy8@gmail.com>
2024-06-03 17:44:09 +0300

    Handle current page activation

--- src/components/profiles.jsx
+++ src/components/profiles.jsx
@@ -1,50 +1,67 @@
-import React, {Component} from 'react';
-import {getProfiles, deleteProfile} from "../services/fakeProfileService";
+import React, { Component } from "react";
+import { getProfiles, deleteProfile } from "../services/fakeProfileService";
+import Pagination from "./common/pagination";
 
 class Profiles extends Component {
-    state = {
-        profiles: getProfiles()
-    }
+  state = {
+    profiles: getProfiles(),
+    pageSize: 4,
+    currentPage: 1,
+  };
 
-    handleDelete = (profile) => {
-        const profiles = this.state.profiles.filter(p => p.id != profile.id)
-        this.setState({profiles})
-    }
+  handleDelete = (profile) => {
+    const profiles = this.state.profiles.filter((p) => p.id !== profile.id);
+    this.setState({ profiles });
+  };
 
-    render() {
-        const {length: count} = this.state.profiles
-        if (count === 0) return <p>There are on profiles in the database</p>
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
 
-        return (
-            <>
-                <p>Showing {count} profiles in the database</p>
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th/>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.profiles.map(profile => (
-                        <tr key={profile.id}>
-                            <td>{profile.first_name}</td>
-                            <td>{profile.last_name}</td>
-                            <td>{profile.email}</td>
-                            <td>
-                                <button onClick={() => this.handleDelete(profile)}
-                                        className="btn btn-danger btn-sm">Delete
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </>
-        );
-    }
+  render() {
+    const { length: count } = this.state.profiles;
+    const { pageSize, currentPage } = this.state;
+    if (count === 0) return <p>There are on profiles in the database</p>;
+
+    return (
+      <>
+        <p>Showing {count} profiles in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.profiles.map((profile) => (
+              <tr key={profile.id}>
+                <td>{profile.first_name}</td>
+                <td>{profile.last_name}</td>
+                <td>{profile.email}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(profile)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={count}
+          currentPage={currentPage}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+        />
+      </>
+    );
+  }
 }
 
 export default Profiles;
