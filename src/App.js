d6daeac826b383f3e8000bd496f91221bb47c4c6
Dar Levy <darlevy8@gmail.com>
2024-06-03 20:38:33 +0300

    Add login form to app routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import ProfileForm from "./components/profileForm";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/profiles/:id" component={ProfileForm} />
           <Route path="/profiles" component={Profiles} />
           <Route path="/not-found" component={NotFound} />
